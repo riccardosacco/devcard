1aad9c5ecab990cfd9bf5ffea75b36bdc32a8f88
Riccardo Sacco <rickybag99@gmail.com>
2020-02-07 15:34:38 +0100

    initial commit

---
+++ assets/js/testimonials.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	
+	$('.testimonial-carousel').owlCarousel({
+        margin:30,
+        nav:false,
+        responsive:{
+	        0:{
+	            items:1,
+	        },
+	        768:{
+	            items:2,
+	        },
+	        1200: {
+		        items:3,
+	        }
+	    }
+	    
+	}); 
+	
+
+
+});
+
+
+
